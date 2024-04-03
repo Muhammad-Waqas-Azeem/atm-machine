7a8cf27b7d5627a931ca5962142e21ca1bb6ac35
Muhammad Waqas Azeem <160093061+Muhammad-Waqas-Azeem@users.noreply.github.com>
2024-04-04 00:31:55 +0500

    added an additional fast cash feature

--- main.js
+++ main.js
@@ -5,16 +5,18 @@ let userEnteredPin = await inquirer.prompt([
     { message: "Enter Your Pin", type: "number", name: "userPin" },
 ]);
 if (userEnteredPin.userPin === pin) {
-    console.log('Correct Pin code');
+    console.log("Correct Pin code");
     console.log(`Your balance is ${balance}`);
     let option = await inquirer.prompt({
         message: "What do you want",
         type: "list",
         name: "choice",
-        choices: ["Cash Withdrawl", "Balance Check"],
+        choices: ["Cash Withdrawl", "Balance Check", "Fast Cash"],
     });
     if (option.choice === "Cash Withdrawl") {
-        let amount = await inquirer.prompt([{ message: "Enter Amount", type: "number", name: "amounts" }]);
+        let amount = await inquirer.prompt([
+            { message: "Enter Amount", type: "number", name: "amounts" },
+        ]);
         if (amount.amounts > balance) {
             console.log("Low Balance");
         }
@@ -24,6 +26,18 @@ if (userEnteredPin.userPin === pin) {
     }
     else if (option.choice === "Balance Check") {
         console.log(`Your balance is ${balance}`);
+    }
+    else if (option.choice === "Fast Cash") {
+        let fastAmount = await inquirer.prompt({
+            name: "amount",
+            message: "Select any fixed amount for fast withdrawl",
+            type: "list",
+            choices: ["500", "1000", "2000", "3000", "4000", "5000"],
+        });
+        console.log(`Your Remaining Balance is ${balance - fastAmount.amount}`);
+    }
+    else {
+        console.log("Invalid amount Please Enter Amount in Numbers");
     }
 }
 else {
--- main.ts
+++ main.ts
@@ -5,30 +5,38 @@ let userEnteredPin = await inquirer.prompt([
   { message: "Enter Your Pin", type: "number", name: "userPin" },
 ]);
 if (userEnteredPin.userPin === pin) {
-  console.log('Correct Pin code');
+  console.log("Correct Pin code");
   console.log(`Your balance is ${balance}`);
-  
-  let option = await inquirer.prompt(
-    {
-      message: "What do you want",
-      type: "list",
-      name: "choice",
-      choices: ["Cash Withdrawl", "Balance Check"],
-    },
-  );
-  if(option.choice === "Cash Withdrawl"){
-    let amount=await inquirer.prompt([{message:"Enter Amount",type:"number",name:"amounts"}]);
-    if(amount.amounts > balance){
-console.log("Low Balance");
-    }
-    else{
+
+  let option = await inquirer.prompt({
+    message: "What do you want",
+    type: "list",
+    name: "choice",
+    choices: ["Cash Withdrawl", "Balance Check", "Fast Cash"],
+  });
+  if (option.choice === "Cash Withdrawl") {
+    let amount = await inquirer.prompt([
+      { message: "Enter Amount", type: "number", name: "amounts" },
+    ]);
+    if (amount.amounts > balance) {
+      console.log("Low Balance");
+    } else {
       console.log(`Your Remaining Balance is ${balance - amount.amounts}`);
     }
-}
-else if(option.choice === "Balance Check"){
+  } else if (option.choice === "Balance Check") {
     console.log(`Your balance is ${balance}`);
+  } else if (option.choice === "Fast Cash") {
+    let fastAmount = await inquirer.prompt({
+      name: "amount",
+      message: "Select any fixed amount for fast withdrawl",
+      type: "list",
+      choices: ["500", "1000", "2000", "3000", "4000", "5000"],
+    });
+    console.log(`Your Remaining Balance is ${balance - fastAmount.amount}`);
+  } else {
+    console.log("Invalid amount Please Enter Amount in Numbers");
   }
-} 
-else {
+}
+ else {
   console.log("Wrong Pin");
 }
