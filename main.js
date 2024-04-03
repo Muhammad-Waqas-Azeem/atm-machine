77b01410c88815bd08624b336746af41e3597d0e
Muhammad Waqas Azeem <160093061+Muhammad-Waqas-Azeem@users.noreply.github.com>
2024-04-04 00:04:06 +0500

    added an else block

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ let balance = 10000;
 let userEnteredPin = await inquirer.prompt([
     { message: "Enter Your Pin", type: "number", name: "userPin" },
 ]);
-if (userEnteredPin.userPin === 2003) {
+if (userEnteredPin.userPin === pin) {
+    console.log('Correct Pin code');
     console.log(`Your balance is ${balance}`);
     let option = await inquirer.prompt({
         message: "What do you want",
@@ -13,8 +14,13 @@ if (userEnteredPin.userPin === 2003) {
         choices: ["Cash Withdrawl", "Balance Check"],
     });
     if (option.choice === "Cash Withdrawl") {
-        let amount = await inquirer.prompt([{ message: "Enter Amount", type: "number", name: "amount" }]);
-        console.log(`Your Remaining Balance is ${balance - amount.amount}`);
+        let amount = await inquirer.prompt([{ message: "Enter Amount", type: "number", name: "amounts" }]);
+        if (amount.amounts > balance) {
+            console.log("Low Balance");
+        }
+        else {
+            console.log(`Your Remaining Balance is ${balance - amount.amount}`);
+        }
     }
     else if (option.choice === "Balance Check") {
         console.log(`Your balance is ${balance}`);
--- main.ts
+++ main.ts
@@ -5,6 +5,7 @@ let userEnteredPin = await inquirer.prompt([
   { message: "Enter Your Pin", type: "number", name: "userPin" },
 ]);
 if (userEnteredPin.userPin === pin) {
+  console.log('Correct Pin code');
   console.log(`Your balance is ${balance}`);
   
   let option = await inquirer.prompt(
@@ -16,8 +17,13 @@ if (userEnteredPin.userPin === pin) {
     },
   );
   if(option.choice === "Cash Withdrawl"){
-    let amount=await inquirer.prompt([{message:"Enter Amount",type:"number",name:"amount"}]);
-    console.log(`Your Remaining Balance is ${balance - amount.amount}`);
+    let amount=await inquirer.prompt([{message:"Enter Amount",type:"number",name:"amounts"}]);
+    if(amount.amounts > balance){
+console.log("Low Balance");
+    }
+    else{
+      console.log(`Your Remaining Balance is ${balance - amount.amount}`);
+    }
 }
 else if(option.choice === "Balance Check"){
     console.log(`Your balance is ${balance}`);
